3180a09052e92237b5fb9f2dafd7249dca2c42f9
IvanTymoshchuk <116722310+IvanTymoshchuk@users.noreply.github.com>
2023-01-11 20:23:59 +0000

    Deploying to gh-pages from @ IvanTymoshchuk/eskimo@86cd27f6483e895b3d5ab58ae2c7c1efe16f4183 🚀

---
+++ index.92553103.js
@@ -0,0 +1,2 @@
+$(".fade").slick({dots:!0,infinite:!0,speed:500,fade:!0,cssEase:"linear"});
+//# sourceMappingURL=index.92553103.js.map
---
+++ index.db2c8b19.js
@@ -0,0 +1,2 @@
+$(".fade").slick({dots:!0,infinite:!0,speed:500,fade:!0,cssEase:"linear"});
+//# sourceMappingURL=index.db2c8b19.js.map
