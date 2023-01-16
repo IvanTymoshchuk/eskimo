d8c25353cdad02c7882b41166170cb46398766a9
IvanTymoshchuk <116722310+IvanTymoshchuk@users.noreply.github.com>
2023-01-16 19:16:55 +0000

    Deploying to gh-pages from @ IvanTymoshchuk/eskimo@2c8ecdd2d6dc594dda3707400ad09778e09d340e 🚀

--- index.5812e672.js
+++
@@ -1,2 +0,0 @@
-$(window).on("resize",(function(){var h=0;$(".hmax").each((function(){$(this).css("height","auto"),$(this).height()>h&&(h=$(this).height())})),$(".hmax").height(h)})).resize();
-//# sourceMappingURL=index.5812e672.js.map
--- index.c6126d0c.js
+++
@@ -1,2 +0,0 @@
-$(window).on("resize",(function(){var h=0;$(".hmax").each((function(){$(this).css("height","auto"),$(this).height()>h&&(h=$(this).height())})),$(".hmax").height(h)})).resize();
-//# sourceMappingURL=index.c6126d0c.js.map
